61692b0bbd4b79b6a99ee73ca5301df00056342b
Henrique-Givisiez <lucogds@gmail.com>
2025-02-26 23:25:03 -0300

    pagina de login feita

--- escola-cerrado/src/App.tsx
+++ escola-cerrado/src/App.tsx
@@ -1,22 +1,35 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/header";
 import AboutUs from "./components/about-us";
 import OurWork from "./components/our-work";
 import Team from "./components/team";
 import Contact from "./components/contact";
 import Footer from "./components/footer";
+import Login from "./components/login"; // Importando a página de login
 import "./index.css";
+
 function App() {
   return (
-    <div>
-      <Header />
-      <main>
-        <AboutUs />
-        <OurWork />
-        <Team />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Header />
+              <main>
+                <AboutUs />
+                <OurWork />
+                <Team />
+                <Contact />
+              </main>
+              <Footer />
+            </div>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
